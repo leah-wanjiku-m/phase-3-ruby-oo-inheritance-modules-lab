1d08417a64d2225f0c32f4fcfc6aa112358fa47c
leah wanjiku <leagm22@gmail.com>
2023-02-14 17:07:46 +0300

    done

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,11 @@
-require 'pry'
+require 'pry'  
 
 class Song
   attr_accessor :name
   attr_reader :artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+
 
   @@songs = []
 
@@ -18,13 +21,6 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
 
   def artist=(artist)
     @artist = artist
